1345c5b2e1b4c2dc45ce7c8303044c6a44ed9b69
James Billcliffe <jbillcliffe@googlemail.com>
2024-10-16 12:55:36 +0000

    added checkout success logic

--- checkout/signals.py
+++ checkout/signals.py
@@ -9,6 +9,7 @@ def update_on_save(sender, instance, created, **kwargs):
     """
     Update order total on lineitem update/create
     """
+    print('update signal')
     instance.order.update_total()
 
 
@@ -17,4 +18,5 @@ def update_on_delete(sender, instance, **kwargs):
     """
     Update order total on lineitem delete
     """
+    print('delete signal')
     instance.order.update_total()
