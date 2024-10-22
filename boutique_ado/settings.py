6e88ce7258a3c9cbdb218480c1a7281571ada66e
James Billcliffe <jbillcliffe@googlemail.com>
2024-10-22 12:03:56 +0000

    Removed Key

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -27,8 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hy#*3ik)=i5fb299pf$eivpw-38#zp!kzcl8qjvxy+wh%!#ga^'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
