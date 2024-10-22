c2dda7fe4f7f3ddd75d26e236cf85f00f11f1590
James Billcliffe <jbillcliffe@googlemail.com>
2024-10-22 11:52:50 +0000

    Heroku Deployment

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -33,7 +33,8 @@ SECRET_KEY = 'django-insecure-hy#*3ik)=i5fb299pf$eivpw-38#zp!kzcl8qjvxy+wh%!#ga^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-jbillcliffe-boutiqueado-jjl9fjjwmvj.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-jbillcliffe-boutiqueado-jjl9fjjwmvj.ws.codeinstitute-ide.net',
+    'jbill-boutique-ado.herokuapp.com', 'localhost']
 
 CSRF_TRUSTED_ORIGINS = ['https://*.codeinstitute-ide.net']
 # Application definition
@@ -135,13 +136,17 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
