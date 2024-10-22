0517e1b50cec28c7ab61817534c9910624a0bcab
James Billcliffe <jbillcliffe@googlemail.com>
2024-10-22 13:10:27 +0000

    Added cache control for AWS

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -196,6 +196,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # Cache Control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'jbill-boutique-ado'
     AWS_S3_REGION_NAME = 'eu-north-1'
@@ -212,6 +218,7 @@ if 'USE_AWS' in os.environ:
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}'
+
 
 # Stripe
 FREE_DELIVERY_THRESHOLD = 50
